9f1cd09e7983eaf07d091b58a6bc204d8966d79c
MADHURYAHAIT <madhurya_202100516@smit.smu.edu.in>
2024-03-12 22:21:12 +0530

    working backend

--- Server/index.js
+++ Server/index.js
@@ -1,25 +1,28 @@
-import { dirname } from "path";
+
 import express from "express";
-import { fileURLToPath } from "url";
+import './mongoDB/connection.js'; 
+import messageModel from "./mongoDB/messageModel.js";
 import bodyParser from "body-parser";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 const app = express();
 const port = 3000; 
-
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get("/", (req, res) => {
-    // res.sendFile(__dirname + "/public/index.html");
-    res.sendFile("app runnin");
-});
-
 app.post("/submit", (req, res) => {
     console.log(req.body);
-    res.send("Data received");
+    res.send("Data received ...");
 });
+
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.get("/", (req, res) => {
+//     // res.sendFile(__dirname + "/public/index.html");
+//     res.sendFile("app runnin");
+// });
+
+// app.post("/submit", (req, res) => {
+//     console.log(req.body);
+//     res.send("Data received");
+// });
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
--- Server/mongoDB/connection.js
+++ Server/mongoDB/connection.js
@@ -1,26 +1,28 @@
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://mady:abc@123@cluster0.i5niiwg.mongodb.net/?retryWrites=true&w=majority";
+import mongoose from "mongoose";
+mongoose.connect("mongodb+srv://mady:abc123456@cluster0.i5niiwg.mongodb.net/sample_supplies?retryWrites=true&w=majority");
+
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
