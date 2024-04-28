a576233077838359577732cf3f614f7131b5f0f7
MADHURYAHAIT <madhurya_202100516@smit.smu.edu.in>
2024-04-28 12:36:58 +0530

    updated icon

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,13 +1,18 @@
+
 import path from 'path';
 import react from '@vitejs/plugin-react';
 
+
 const SRC_DIR = path.resolve(__dirname, './src');
 const PUBLIC_DIR = path.resolve(__dirname, './public');
-const BUILD_DIR = path.resolve(__dirname, './www');
-
+const BUILD_DIR = path.resolve(__dirname, './www',);
 export default async () => {
-  return {
-    plugins: [react()],
+
+  return  {
+    plugins: [
+      react(),
+
+    ],
     root: SRC_DIR,
     base: '',
     publicDir: PUBLIC_DIR,
@@ -27,5 +32,6 @@ export default async () => {
     server: {
       host: true,
     },
+
   };
-};
+}
