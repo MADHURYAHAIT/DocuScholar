bb5daa25ef029b1f6c39b37934dfe63915e78758
MADHURYAHAIT <madhurya_202100516@smit.smu.edu.in>
2024-05-12 11:30:57 +0530

    Connection url

--- Server/mongoDB/connection.js
+++ Server/mongoDB/connection.js
@@ -1,11 +1,12 @@
-require('dotenv').config(); // Load environment variables from .env file
+require('dotenv').config();
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.URI, {
+const uri = process.env.MONGODB_URI;
+
+mongoose.connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
 .then(() => console.log("Connected to MongoDB"))
 .catch(error => console.error("Error connecting to MongoDB:", error));
 
-module.exports = mongoose.connection;
